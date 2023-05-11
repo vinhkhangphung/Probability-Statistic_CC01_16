cb3241452a08c0780630cf75bd8d423293e32e2a
Khang Phung <vinhkhangphung@gmail.com>
2023-05-11 23:11:03 +0700

    add requirement.R

---
+++ requirement.R
@@ -0,0 +1,8 @@
+# Install required packages
+install.packages(c("exactRankTests", "coin", "survival", "mice"))
+
+# Load installed packages
+library(exactRankTests)
+library(coin)
+library(survival)
+library(mice)
